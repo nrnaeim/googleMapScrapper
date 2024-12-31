43182e6852dfa2a077c291c1992446dbb70a7105
Niemur Rahman <nrnaeim@gmail.com>
2025-01-01 00:53:00 +0600

    userInput.js and geolocations.json file is created and ignoring gelocation.json due geolocation is big file

---
+++ userInputData/userInput.js
@@ -0,0 +1,13 @@
+/*
+ * Title: User Input
+ * Description: Taking user desire location and catagory for collecting puplic data
+ * Author: Niemur Rahman
+ * Date: 01/01/2025
+ */
+//dependencies
+
+//module scaffolding
+export const userInput = {};
+userInput.countryCode = "GB";
+userInput.locationType = "town";
+userInput.searchItems = ["taxi farms", "mini cab firms", "chauffer firms"];
